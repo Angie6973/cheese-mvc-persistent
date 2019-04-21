61619752b57e960513bbf7bb9369339d348de1f8
Angelika Stephanie Pierce <angiekinter@gmail.com>
2019-04-20 23:28:59 -0500

    add cheeses to category cheese type changed to category update cheesecontroller close to finish part 2

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -14,17 +14,21 @@ public class Category {
     @GeneratedValue
     private int id;
 
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name ="category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
     public Category() {
+
     }
 
     public int getId() {
         return id;
     }
-
-    @NotNull
-    @Size(min=3, max=15)
-    private String name;
-
     public String getName() {
         return name;
     }
@@ -32,4 +36,7 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+    public List<Cheese> getCheeses() {
+
+        return cheeses;}
 }
